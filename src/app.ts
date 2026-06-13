602a550cdea90c04a873553fa5f15c44424b93d3
Chalex <177287665+chachaa10@users.noreply.github.com>
2026-06-13 17:25:04 +0800

    add morgan logger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { authRouter } from "@/routes/auth.routes";
 import cookieParser from "cookie-parser";
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
